6254902e90779d204caf3846b14fe405ea9ac4fb
isarnf <isarnf@gmail.com>
2022-12-09 10:18:59 -0300

    adjusting data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into veterinarios values (1, "Isabela", "123.456.789-00", "CRMV01", "isa@gmail.com");
-insert into veterinarios values (2, "Lucas", "987.654.321-00", "CRMV02", "lucas@gmail.com");
+insert into veterinarios values (1, "Isabela", "123.456.789-00", "CRMV01", "isa@gmail.com", "123");
+insert into veterinarios values (2, "Lucas", "987.654.321-00", "CRMV02", "lucas@gmail.com", "456");
 
 insert into animais values (1, "Rex", "Canis lupus familiaris", "03/05/2018", "macho", "8.5 kg", "Vira-lata");
 insert into animais values (2, "Nina", "Felis catus", "29/12/2021", "fêmea", "4.2 kg", "Persa");
@@ -7,5 +7,5 @@ insert into animais values (2, "Nina", "Felis catus", "29/12/2021", "fêmea", "4
 insert into clientes values (1, "111.111.111-11", "Marília dos Santos", "(16) 99921-1111", "marilia@gmail.com");
 insert into clientes values (2, "222.222.222-22", "Roberto Carvalho", "(16) 99972-2222", "rc@gmail.com");
 
-insert into consultas values (1, 1, 1, "2022-06-05T10:20:40.431Z");
-insert into consultas values (2, 2, 2, "2022-08-24T14:35:00.526Z");
+insert into consultas values (1, 1, 1, "2022-06-05T10:20:40.431Z", "ABERTO");
+insert into consultas values (2, 2, 2, "2022-08-24T14:35:00.526Z", "ABERTO");
